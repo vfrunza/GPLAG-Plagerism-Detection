d48945bb806fd940ab64c69e261388d2546e6d11
Victor Frunza <victor.frunza96@gmail.com>
2021-11-10 21:36:25 -0800

    Delete Data/src/A2017/Z1/Z1 directory

--- Data/src/A2017/Z1/Z1/student2447.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.>
-int main() {
-	int i,j,a,b,c;
-	char H='H',C='C',p='P';
-	
-	return 0;
-}
--- Data/src/A2017/Z1/Z1/student7085.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Najjeftinije je u \"Trovacu \"");
-	//printf("Citiram: \"Ja sam bio...\"");
-	return 0;
-}
